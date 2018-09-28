b1a8470810af56a4b70437d9d7598b1c66107ff8
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-28 15:29:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array =[]
+  array =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   array.each do |name|
     "Hello, my name is #{name}."
   end
