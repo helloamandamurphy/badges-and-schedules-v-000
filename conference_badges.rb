8f9fce6b9709c855145834d71b1212e776eb8a06
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-28 16:15:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,9 @@ end
 def assign_rooms(attendees)
   assignment = Array.new
   attendees.each_with_index do |name,index|
-    assignment.push(badge_maker(name), index + 1)
+    assignment.push(name, index + 1)
   end
 assignment
-  #puts "Hello, #{assignment[0][0]}! You'll be assigned to room #{assignment[0][1]}!"
 end
 
 def printer
